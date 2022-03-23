b043ceeb3395a07f47f4aedd919e784baff7a9c6
Kitaloko <victor.takeshi1010@gmail.com>
2022-03-23 15:16:03 -0300

    adicionar comida

--- script4.js
+++ script4.js
@@ -7,6 +7,10 @@ snake[0] = {
     y: 8*box
 }
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random()*15 + 1)*box,
+    y: Math.floor(Math.random()*15 + 1)*box
+}
 
 function criarBG(){
     context.fillStyle = "lightblue";
@@ -20,13 +24,18 @@ function criarCobrinha(){
     }
 }
 
-document.addEventListener('keydown', update);
+function drawFood(){
+    context.fillStyle = "orange";
+    context.fillRect(food.x, food.y, box, box);
+}
+
+document.addEventListener("keydown", update);
 
 function update (event){
-    if (event.keycode == 37 && direction != "right") direction = "left";
-    if (event.keycode == 38 && direction != "down") direction = "up";
-    if (event.keycode == 39 && direction != "left") direction = "right";
-    if (event.keycode == 40 && direction != "up") direction = "down";
+    if (event.keyCode == 37 && direction != "right") direction = "left";
+    if (event.keyCode == 38 && direction != "down") direction = "up";
+    if (event.keyCode == 39 && direction != "left") direction = "right";
+    if (event.keyCode == 40 && direction != "up") direction = "down";
 }
 
 function iniciarJogo(){
@@ -37,6 +46,7 @@ function iniciarJogo(){
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x; 
     let snakeY = snake[0].y;
